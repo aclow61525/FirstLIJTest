d0c6b130f1e5737fc4349c70625176c0102f30f4
Adam <aclow.61525@farnborough.ac.uk>
2017-09-14 11:39:23 +0100

    Changed the first message Added a second message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello this is Adam");
+        System.out.println("Hello World");
+        System.out.println("This is Adam");
     }
 }
